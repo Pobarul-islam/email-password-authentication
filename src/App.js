43a69887f384a8dc6cb10e19f75a1ac00a4a1f75
Pobarul-islam <pobarul3@gmail.com>
2022-04-08 12:10:24 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import { getAuth } from "firebase/auth";
 import "./App.css";
 import app from "./firebase.init";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const auth = getAuth(app);
 
 function App() {
-  const handleEmailChange = event => {
+  const handleEmailBlur = event => {
     console.log(event.target.value);
+  }
+  
+  const handlePasswordBlur = event => {
+    console.log(event.target.value)
+  }
+
+  const handleFormSubmit = event => {
+    console.log('form submited')
+    event.preventDefault();
   }
   return (
     <div className="App">
-      <form>
-        <input onChange={handleEmailChange} type="email" name="" id="" />
-        <input type="password" name="" id="" />
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmailBlur} type="email" name="" id="" /> <br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" /> <br />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
